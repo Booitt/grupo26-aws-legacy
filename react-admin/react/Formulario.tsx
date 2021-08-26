100e4e9e7e18df0905f5d84414454af823fe0134
Jessica Biali Gelman <bialigelman@gmail.com>
2021-08-26 17:52:37 -0300

    primeira parte da home e formulario de leads

--- react-admin/react/Formulario.tsx
+++ react-admin/react/Formulario.tsx
@@ -1,24 +1,53 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import axios from 'axios'
 import style from './style.module.css'
 
+interface FormularioProps{
+  width: number;
+  height: number;
+}
 
+const Formulario: StorefrontFunctionComponent<FormularioProps> = () => {
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('')
+  const [email, setEmail] = useState('')
+  
+  const handleNome = (e:React.ChangeEvent<any>) =>{ 
+    setNome(e.target.value)
+  }
 
-const Formulario = () => {
+  const handleTelefone = (e: React.ChangeEvent<any>) =>{ 
+    setTelefone(e.target.value)
+  }
+
+  const handleEmail = (e: React.ChangeEvent<any>) =>{ 
+    setEmail(e.target.value)
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault;
+    const data = {
+      "nome": `${nome}`,
+      "telefone": `${telefone}`,
+      "email": `${email}`
+    };
+    axios.post('https://7slql6j8i0.execute-api.us-east-2.amazonaws.com/leads', data)
+          .catch((err) => console.log(err.message))
+  }
 
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={handleSubmit}>
       <div className={style.inputs}>
-        <label className={style.label}>Nome</label>
-        <input className={style.input} type="text"/>
+        <label className={style.label}>Name</label>
+        <input className={style.input} type="text" required onChange={handleNome} value={nome}/>
       </div>
       <div className={style.inputs}>
-        <label className={style.label}>Telefone</label>
-        <input className={style.input} type="text"/>
+        <label className={style.label}>Phone</label>
+        <input className={style.input} type="text" onChange={handleTelefone} value={telefone}/>
       </div>
       <div className={style.inputs}>
         <label className={style.label}>E-mail</label>
-        <input className={style.input} type="email" />
+        <input className={style.input} type="email" required onChange={handleEmail} value={email}/>
         <input className={style.submit} type="submit" value="Cadastrar" />
       </div>
     </form>
